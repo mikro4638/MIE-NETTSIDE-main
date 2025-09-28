f86930cf047c1f39ca9e3347c6d4a0443950ec6e
Mie.Kronkvist <mie.kronkvist@student.uib.no>
2025-09-28 13:53:39 +0200

    Rettet bildenavn og la til riktige filer i public/

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  base: '/MIE-NETTSIDE-main/',   // 👈 viktig for GitHub Pages
-  outDir: './dist',              // standard build-mappe
-});
+export default defineConfig({});
